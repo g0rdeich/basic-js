510913c001f6ce422781e5258cc82bd31204803d
Alex Gordeev <gordeeffff@gmail.com>
2020-09-27 19:15:00 +0300

    feat: solve carbon-dating and dream team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,22 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  //throw new CustomError('Not implemented');
+  let result = '';
+  if (typeof members !== 'object' || members === null) {
+    return false;
+  }
+  else if(members.length === 0) {
+    return null;
+  }
+  else {
+    for(let i=0; i<members.length; i++) {
+      if(typeof members[i] === 'string') {
+        let newWord = members[i].trim();
+        let letter = newWord[0].toUpperCase();
+        result += letter;
+      }
+    }
+  }
+  return Array.from(result).sort().join('');
 };
