d867417be528201e8b67e0c1bc8df90fce41ee9e
Alex Gordeev <gordeeffff@gmail.com>
2020-09-29 20:05:39 +0300

    feat: solve transform array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,44 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function transform(arr) {
+  let commands = ['--discard-next','--discard-prev','--double-next','--double-prev'];
+  let res = [];
+  if(typeof arr !== 'object') {
+    throw new Error;
+  }
+  else {
+    for (let i = 0; i < arr.length; i++) {
+      if(commands.includes(arr[i])) {
+        let command = arr[i];
+        switch (command) {
+          case '--discard-next':
+            if(i+1 < arr.length) {
+            i++;
+            }
+            break
+          case '--discard-prev':
+            if(i>0 && arr[i-2] !== '--discard-next') {
+            res.pop();
+            }
+            break
+          case '--double-next':
+            if(i+1 < arr.length) {
+            res.push(arr[i+1]);
+            }
+            break
+          case '--double-prev':
+            if(i>0 && arr[i-2] !== '--discard-next') {
+            res.push(arr[i-1]);
+            }
+            break
+          default:
+            break
+        }
+      }
+      else {
+        res.push(arr[i]);
+      }
+    }
+  }
+  return res;
+}
