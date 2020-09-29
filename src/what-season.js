5185a06840e4ff236cc9885f2e4dcca9c357c1a3
Alex Gordeev <gordeeffff@gmail.com>
2020-09-29 15:16:48 +0300

    feat: solve season

--- src/what-season.js
+++ src/what-season.js
@@ -1,6 +1,27 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function getSeason(/* date */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function getSeason(date) {
+  let res;
+  if(Object.prototype.toString.call(date) === '[object Date]') {
+    let month = date.getMonth();
+    if((month >= 0 && month <= 1) || month === 11) {
+      res = 'winter';
+    }
+    else if(month >= 2 && month <= 4) {
+      res = 'spring';
+    }
+    else if(month >= 5 && month <= 7) {
+      res = 'summer';
+    }
+    else  {
+      res = 'autumn';
+    }
+  }
+  else if(date === undefined) {
+    res = 'Unable to determine the time of year!';
+  }
+  else {
+    throw new Error;
+  }
+  return res;
+}
